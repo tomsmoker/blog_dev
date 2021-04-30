f170ad3a42cd5e01bc9ec333ff1f3ad73d96d803
tomsmoker <tomsmoker-uwa@gmail.com>
2021-04-30 17:27:30 +0800

    Working registration page

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,37 @@
-from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField, BooleanField, SubmitField 
-from wtforms.validators import DataRequired 
+from flask_wtf          import FlaskForm 
+from wtforms            import StringField, PasswordField, BooleanField, SubmitField 
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo 
+from app.models         import User
 
 class LoginForm(FlaskForm):
 
     username = StringField('username', validators = [DataRequired()])
     password = PasswordField('password', validators = [DataRequired()])
     remember_me = BooleanField('remember me')
-    submit = SubmitField('done')
+    submit = SubmitField('all done')
+
+class RegistrationForm(FlaskForm):
+
+    username = StringField('username', validators = [DataRequired()])
+    email = StringField('email', validators = [DataRequired(), Email()])
+    password = PasswordField('password', validators = [DataRequired()])
+    password_again = PasswordField(
+        'password again', 
+        validators = [DataRequired(), EqualTo('password')])
+    submit = SubmitField('all done')
+
+    def validate_username(self, username):
+
+        user = User.query.filter_by(username = username.data).first()
+
+        if user is not None:
+            raise ValidationError("username is taken. roll again.")
+
+    def validate_email(self, email):
+
+        user = User.query.filter_by(email = email.data).first()
+
+        if user is not None:
+            raise ValidationError("this email is already in our system")
 
     
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,13 @@
-from flask import render_template, flash, redirect, url_for
-from app import app 
-from app.forms import LoginForm
+from flask         import render_template, flash, redirect, url_for, request
+from app           import app, db
+from app.forms     import LoginForm, RegistrationForm
+from flask_login   import current_user, login_user, logout_user, login_required
+from app.models    import User
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     
     user = {'username': 'tomsmoker'}
@@ -19,19 +23,60 @@ def index():
         }
     ]
 
-    return render_template('index.html', title = "home", user = user, posts = posts)
+    return render_template('index.html', title = "home", posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
 
     form = LoginForm()
 
     if form.validate_on_submit():
 
-        flash("user {} wants to login, do they want to be remembered? {}".format(
-            form.username.data, form.remember_me.data
-        ))
+        user = User.query.filter_by(username = form.username.data).first()
 
-        return redirect(url_for('index'))
+        if user is None or not user.check_password(form.password.data):
+            flash("Yeah, no.  Either the username or the password is busted.")
+            return redirect(url_for('login'))
+
+        login_user(user, remember = form.remember_me.data)
+
+        next_page = request.args.get('next')
+
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+
+        return redirect(next_page)
 
     return render_template('login.html', title = 'sign in', form = form)
+
+@app.route('/logout')
+def logout():
+
+    logout_user()
+
+    return redirect(url_for('index'))
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+
+        user = User(username = form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('welcome')
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html', title = 'register', form = form)
